ea52b76b37af4d6d7f444d3653bf8da4e8498554
WayneMusungu <waynemuyera17@gmail.com>
2022-07-28 16:17:56 +0300

    Create a template and its view function that will return all news to china

--- newsapi/views.py
+++ newsapi/views.py
@@ -35,3 +35,34 @@ def index(request):
     }
         
     return render (request, "index.html", context)
+
+def china(request):
+    
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+    
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={wayne.NEWS_API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+        
+    elif category:
+        url = f'https://newsapi.org/v2/top-headlines?country=cn&category={category}&apiKey={wayne.NEWS_API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+        
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?country=cn&apiKey={wayne.NEWS_API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+        
+        
+        
+    context = {
+        'articles' : articles
+    }
+        
+    return render (request, "china.html", context)
