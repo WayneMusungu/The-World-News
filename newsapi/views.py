97edb05b455d6124d813dcda2f5bc7b6458b691c
WayneMusungu <waynemuyera17@gmail.com>
2022-07-28 16:41:53 +0300

    Create a template and its view function that will return all news to austria

--- newsapi/views.py
+++ newsapi/views.py
@@ -129,3 +129,35 @@ def argentina(request):
     }
         
     return render (request, "argentina.html", context)
+
+
+def austria(request):
+    
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+    
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={wayne.NEWS_API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+        
+    elif category:
+        url = f'https://newsapi.org/v2/top-headlines?country=at&category={category}&apiKey={wayne.NEWS_API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+        
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?country=at&apiKey={wayne.NEWS_API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+        
+        
+        
+    context = {
+        'articles' : articles
+    }
+        
+    return render (request, "austria.html", context)
