5b5afb80d8e8178fc2e10e6d068ded5b85eaed66
WayneMusungu <waynemuyera17@gmail.com>
2022-07-28 15:15:30 +0300

    Add an elif statement that will search for category types

--- newsapi/views.py
+++ newsapi/views.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.shortcuts import render, redirect
 import requests
 import newsapi.wayne as wayne
@@ -7,9 +8,16 @@ import newsapi.wayne as wayne
 def index(request):
     
     country = request.GET.get('country')
+    category = request.GET.get('category')
     
     if country:
         url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={wayne.NEWS_API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+        
+    elif category:
+        url = f'https://newsapi.org/v2/top-headlines?country=us&category={category}&apiKey={wayne.NEWS_API_KEY}'
         response = requests.get(url)
         data = response.json()
         articles = data['articles']
@@ -19,6 +27,8 @@ def index(request):
         response = requests.get(url)
         data = response.json()
         articles = data['articles']
+        
+        
         
     context = {
         'articles' : articles
