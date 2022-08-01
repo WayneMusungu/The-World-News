88338174a1cf53027b6e52f62b599d9b00efe74e
WayneMusungu <waynemuyera17@gmail.com>
2022-08-01 09:51:54 +0300

    make changes to templates

--- newsapi/urls.py
+++ newsapi/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('usa/',views.index, name='index'),
     path('china/',views.china, name='china'),
     path('united-arab-emirates/',views.uae, name='uae'),
     path('argentina/',views.argentina, name='argentina'),
